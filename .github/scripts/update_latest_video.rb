a994f2718d2810864be180492b671f765f2624a3
Harcipan <92209277+Harcipan@users.noreply.github.com>
2024-01-11 07:21:07 +0100

    Update update_latest_video.rb

--- .github/scripts/update_latest_video.rb
+++ .github/scripts/update_latest_video.rb
@@ -16,12 +16,25 @@ end
 api_key = ENV['YOUTUBE_API_KEY']
 channel_id = ENV['CHANNEL_ID']
 
+puts "API Key: #{api_key}"
+puts "Channel ID: #{channel_id}"
+
 latest_video_id = get_latest_video_id(api_key, channel_id)
 
 if latest_video_id
   latest_video_url = "https://www.youtube.com/watch?v=#{latest_video_id}"
+
+  # Debug - Print existing content of latest_video.json
+  existing_content = File.read('_data/latest_video.json')
+  puts "Existing Content of latest_video.json:\n#{existing_content}"
+
   File.write('_data/latest_video.json', { 'url' => latest_video_url }.to_json)
+
   puts "Updated latest video URL: #{latest_video_url}"
+
+  # Debug - Print updated content of latest_video.json
+  updated_content = File.read('_data/latest_video.json')
+  puts "Updated Content of latest_video.json:\n#{updated_content}"
 else
   puts "No videos found."
 end
