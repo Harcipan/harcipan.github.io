7eaa59982bdc8927adfd92dbb983aae0a9cb5525
harcipan <ifj.kornel.kemecsei@gmail.com>
2024-01-13 12:16:27 +0100

    KB latest Vid

--- .github/scripts/update_latest_video.rb
+++ .github/scripts/update_latest_video.rb
@@ -14,27 +14,45 @@ def get_latest_video_id(api_key, channel_id)
 end
 
 api_key = ENV['YOUTUBE_API_KEY']
-channel_id = ENV['CHANNEL_ID']
+hp_channel_id = ENV['HP_CHANNEL_ID']
+kb_channel_id = ENV['KB_CHANNEL_ID']
 
 puts "API Key: #{api_key}"
-puts "Channel ID: #{channel_id}"
 
-latest_video_id = get_latest_video_id(api_key, channel_id)
+# Read existing content of the file
+existing_content = {}
+if File.exist?('_data/latest_video.json')
+  existing_content = JSON.parse(File.read('_data/latest_video.json'))
+end
 
-if latest_video_id
-  latest_video_url = "https://www.youtube.com/watch?v=#{latest_video_id}"
+# Fetch latest video for Harcipan
+latest_video_id_hp = get_latest_video_id(api_key, hp_channel_id)
 
-  # Debug - Print existing content of latest_video.json
-  existing_content = File.read('_data/latest_video.json')
-  puts "Existing Content of latest_video.json:\n#{existing_content}"
+if latest_video_id_hp
+  latest_video_url_hp = "https://www.youtube.com/watch?v=#{latest_video_id_hp}"
 
-  File.write('_data/latest_video.json', { 'url' => latest_video_url }.to_json)
+  # Update the existing content with new information
+  existing_content['hp_id'] = latest_video_id_hp
 
-  puts "Updated latest video URL: #{latest_video_url}"
-
-  # Debug - Print updated content of latest_video.json
-  updated_content = File.read('_data/latest_video.json')
-  puts "Updated Content of latest_video.json:\n#{updated_content}"
+  # Write the updated content back to the file
+  File.write('_data/latest_video.json', existing_content.to_json)
+  puts "Updated latest video for Harcipan id: #{latest_video_url_hp}"
 else
-  puts "No videos found."
+  puts "No videos found for Harcipan."
+end
+
+# Fetch latest video for Kriszbubu
+latest_video_id_kb = get_latest_video_id(api_key, kb_channel_id)
+
+if latest_video_id_kb
+  latest_video_url_kb = "https://www.youtube.com/watch?v=#{latest_video_id_kb}"
+
+  # Update the existing content with new information
+  existing_content['kb_id'] = latest_video_id_kb
+
+  # Write the updated content back to the file
+  File.write('_data/latest_video.json', existing_content.to_json)
+  puts "Updated latest video for Kriszbubu id: #{latest_video_url_kb}"
+else
+  puts "No videos found for Kriszbubu."
 end
